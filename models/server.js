61521ec635738de7c1abb316a4dfba7b78aad393
tucno21 <carlitostucno@gmail.com>
2023-06-27 20:16:30 -0500

    cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
     constructor() {
@@ -21,6 +22,9 @@ class Server {
     middlewares() {
         //desplegar el directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        //cors
+        this.app.use(cors());
     }
 
     configSockets() {
